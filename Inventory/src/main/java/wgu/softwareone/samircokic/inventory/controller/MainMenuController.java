92eac4c652afe83cc50127197d32cd67a429ad38
worhol <trashuries@gmail.com>
2022-09-02 13:50:22 -0500

    Updated methods to add, modify products/parts

--- Inventory/src/main/java/wgu/softwareone/samircokic/inventory/controller/MainMenuController.java
+++ Inventory/src/main/java/wgu/softwareone/samircokic/inventory/controller/MainMenuController.java
@@ -2,10 +2,14 @@ package wgu.softwareone.samircokic.inventory.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -28,7 +32,11 @@ public class MainMenuController implements Initializable {
     }
 
     @FXML
-    public void onActionModifyProduct(ActionEvent actionEvent) {
+    public void onActionModifyProduct(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/wgu/softwareone/samircokic/inventory/ModifyProductMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
@@ -40,16 +48,27 @@ public class MainMenuController implements Initializable {
     }
 
     @FXML
-    public void onActionAddProduct(ActionEvent actionEvent) {
+    public void onActionAddProduct(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/wgu/softwareone/samircokic/inventory/AddProductMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    public void onActionAddPart(ActionEvent actionEvent) {
-
+    public void onActionAddPart(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/wgu/softwareone/samircokic/inventory/AddPartMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    public void onActionModifyPart(ActionEvent actionEvent) {
+    public void onActionModifyPart(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/wgu/softwareone/samircokic/inventory/ModifyPartMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
