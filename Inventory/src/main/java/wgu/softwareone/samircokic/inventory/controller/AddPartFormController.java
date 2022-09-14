4daaec64e68a0d5734f342a0d68a6c4fc8f07e81
worhol <trashuries@gmail.com>
2022-09-14 15:33:17 -0500

    Saving work, addpartformcontroller javadoc finished

--- Inventory/src/main/java/wgu/softwareone/samircokic/inventory/controller/AddPartFormController.java
+++ Inventory/src/main/java/wgu/softwareone/samircokic/inventory/controller/AddPartFormController.java
@@ -1,7 +1,5 @@
 package wgu.softwareone.samircokic.inventory.controller;
-/**
- * @author Samir Cokic
- */
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -19,11 +17,14 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * @author Samir Cokic
+ *
+ * <p>This class runs the logic of adding a part in GUI. It also implements Initializable interface </p>
+ */
 public class AddPartFormController implements Initializable {
     @FXML
     private RadioButton outRBtn;
-    @FXML
-    private ToggleGroup madein;
     @FXML
     private RadioButton inRBtn;
     @FXML
@@ -47,9 +48,10 @@ public class AddPartFormController implements Initializable {
     Parent scene;
 
     /**
-     *
-     * @param url
-     * @param resourceBundle
+     *<p>Called to initialize a controller after its root element has been completely processed.
+     * Refer to <a href="https://docs.oracle.com/javase/8/javafx/api/javafx/fxml/Initializable.html">Oracle</a> </p>
+     * @param url The location used to resolve relative paths for the root object, or null if the location is not known.
+     * @param resourceBundle The resources used to localize the root object, or null if the root object was not localized
      */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -57,7 +59,13 @@ public class AddPartFormController implements Initializable {
     }
 
     /**
-     * @param actionEvent
+     * <p>This method that takes the ActionEvent object as the argument,
+     * when called switches the label in AddPart GUI,based on whether the radio button
+     * for in house or outsourced was chosen.It checks the whether the outRbtn or inRBtn was selected and then switches the label value
+     * between Company Name for outsourced product,
+     * and Machine ID for in house made product</p>
+     *
+     * @param actionEvent the event that calls the method
      */
     @FXML
     public void inHouseOrOutsourcedMode(ActionEvent actionEvent) {
@@ -71,8 +79,9 @@ public class AddPartFormController implements Initializable {
     }
 
     /**
-     *
-     * @return
+     *<p>This method runs through allParts list and checks for the largest number of id in it.
+     * Then it adds 1 to that largest number and assign the value to new id</p>
+     * @return the id number
      */
     public static int createID() {
         int id = 1;
@@ -86,7 +95,13 @@ public class AddPartFormController implements Initializable {
     }
 
     /**
-     * @return
+     * <p>This method checks if the min value is less than max value and that inventory value is in between those two.
+     * It returns true if condition is right and false otherwise.<br>
+     * <b>RUNTIME ERROR</b> The issues with this method were if the inappropriate data was entered for example
+     * String value instead of int value. In order to avoid that, the exception handling was used that warns the user
+     * about the inappropriate data entered</p>
+     *
+     * @return the boolean true or false
      */
     public boolean minIsLessThanMax() {
         try {
@@ -98,11 +113,33 @@ public class AddPartFormController implements Initializable {
             }
 
         } catch (NumberFormatException numberFormatException) {
-
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Inappropriate Data");
+            alert.setContentText("Please enter valid data for each field");
+            alert.showAndWait();
         }
         return true;
     }
 
+    /**
+     * <p>This method upon receiving ActionEvent object as the argument, saves the entered values of the created part and moves to the main menu.
+     * It first checks which radio button is called. If it's a inRBtn that means part is being made in house so it
+     * takes the value of id, name, inventory, price min and max to create a part, but since it's a in house created part
+     * it also takes the value of machine ID. Then it calls the minIsLessThanMax() method and if that method returns true
+     * part is created and added to allParts Inventory list. After the save button was clicked, the method takes the user to main menu
+     * If the outRBtn was chosen then the outsourced part was created, which works the same as the in house method except instead of int machine id
+     * method uses company name String.
+     * The method also warns user if the inventory value is not in between min and max values, and if the min is larger than max.<br>
+     *
+     * <b>RUNTIME ERROR</b> The issues that were coming with this method were ClassCastException.
+     * Basically the method was trying to pass the instance of InHouse object or a Outsource one and add it to the allParts
+     * which was looking for a Part object.
+     * The solution was to use polymorphism and create the Part object that would downcast as either InHouse or Outsourced object
+     * once its being added to allParts Inventory</p>
+     *
+     * @param actionEvent the event that calls the method
+     * @throws IOException checks for the IllegalArgumentException if the user entered invalid values.
+     */
     @FXML
     public void savePart(ActionEvent actionEvent) throws IOException {
         try {
@@ -163,6 +200,11 @@ public class AddPartFormController implements Initializable {
         }
     }
 
+    /**
+     * <p>This method takes the user back to the Main Menu</p>
+     * @param actionEvent the event that calls the method
+     * @throws IOException checks for the IllegalArgumentException if the user entered invalid values.
+     */
     @FXML
     public void displayMainMenu(ActionEvent actionEvent) throws IOException {
         stage = (Stage) ((Button) actionEvent.getSource()).getScene().getWindow();
